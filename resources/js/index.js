131926614a043cea6eb5ad9a74a264c308c61ee1
Eider22 <eidercardona22@gmail.com>
2022-11-16 15:59:17 -0500

    Se hacen algunos cambios en nav -Se mejora el nav - Se aplica una transiciÃ³n al nav - Se aplica opacidad al main cuando se abre el nav

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const burger = document.querySelector("#burger")
-    const options = document.querySelector("#opti")
-    
-    burger.addEventListener('click',() => {
-        console.log("sisas")
-        options.classList.toggle("hidden")
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector("#burger");
+  const options = document.querySelector("#nav");
+  const main = document.getElementById("main");
+  const html = document.querySelector("html");
 
+  burger.addEventListener("click", () => {
+    options.classList.toggle("active");
+    main.classList.toggle("main-disabled");
+    html.classList.toggle("block-scroll");
+  });
+
+  document.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
