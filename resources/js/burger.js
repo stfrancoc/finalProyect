a1854e2b7e753d18ffeb6018a71ed7dd09615b6c
Eider22 <eidercardona22@gmail.com>
2022-12-12 15:28:43 -0500

    Se agregra el burger-close-icon

--- resources/js/burger.js
+++ resources/js/burger.js
@@ -2,17 +2,32 @@ const activeBurger = (
   burgerIdSelector,
   optionsMenuIdSelector,
   mainIdSelector,
-  htmlTagSelector
+  htmlTagSelector,
+  burgerCloseIdSelector
 ) => {
   const burger = document.getElementById(burgerIdSelector);
+  const burgerClose = document.getElementById(burgerCloseIdSelector);
   const options = document.getElementById(optionsMenuIdSelector);
   const main = document.getElementById(mainIdSelector);
   const html = document.querySelector(htmlTagSelector);
+  const plusIcon = document.getElementById("plus-icon");
 
   burger.addEventListener("click", () => {
     options.classList.toggle("active");
     main.classList.toggle("main-disabled");
     html.classList.toggle("block-scroll");
+    burger.classList.toggle("none")
+    burgerClose.classList.toggle("none")
+    plusIcon.classList.toggle("none");
+  });
+
+  burgerClose.addEventListener("click", () => {
+    options.classList.toggle("active");
+    main.classList.toggle("main-disabled");
+    html.classList.toggle("block-scroll");
+    burger.classList.toggle("none")
+    burgerClose.classList.toggle("none")
+    plusIcon.classList.toggle("none");
   });
 };
 
--- resources/js/index.js
+++ resources/js/index.js
@@ -2,6 +2,6 @@ import activeBurger from "./burger.js";
 import quitar from "./quitar.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  activeBurger("burger", "nav", "main", "html");
+  activeBurger("burger", "nav", "main", "html", "burger-close");
   const quita = document.addEventListener("click",(e)=>{quitar(e)});
 });
